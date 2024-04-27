84b8899d91f2d2254a48ba8375999effd9c0865e
Zia Roque <zia.roqs@gmail.com>
2024-04-27 04:06:54 -0400

    test: Upload Mocha test files for problems 1-3

---
+++ test/01-sum-range-test.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const sumRange = require('../problems/01-sum-range.js');
+
+describe('sumRange', () => {
+  it('tests if sumRange(3) returns 6', () => {
+    const num = 3;
+    const expected = 6;
+
+    const result = sumRange(num);
+
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if sumRange(2) returns 3', () => {
+    const num = 2;
+    const expected = 3;
+
+    const result = sumRange(num);
+
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if sumRange(1) returns 1', () => {
+    const num = 1;
+    const expected = 1;
+
+    const result = sumRange(num);
+
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if sumRange(0) returns 0', () => {
+    const num = 0;
+    const expected = 0;
+
+    const result = sumRange(num);
+
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if sumRange(-7) returns null', () => {
+    const num = -7;
+    const expected = null;
+
+    const result = sumRange(num);
+
+    assert.strictEqual(result, expected);
+  });
+});
---
+++ test/02-power-test.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const power = require('../problems/02-power.js');
+
+describe('power', () => {
+  it('tests if power(2, 4) returns 16', () => {
+    const base = 2;
+    const exponent = 4;
+    const expected = 16;
+    const result = power(base, exponent);
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if power(2, 3) returns 8', () => {
+    const base = 2;
+    const exponent = 3;
+    const expected = 8;
+    const result = power(base, exponent);
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if power(2, 2) returns 4', () => {
+    const base = 2;
+    const exponent = 2;
+    const expected = 4;
+    const result = power(base, exponent);
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if power(2, 1) returns 2', () => {
+    const base = 2;
+    const exponent = 1;
+    const expected = 2;
+    const result = power(base, exponent);
+    assert.strictEqual(result, expected);
+  });
+
+  it('tests if power(2, 0) returns 1', () => {
+    const base = 2;
+    const exponent = 0;
+    const expected = 1;
+    const result = power(base, exponent);
+    assert.strictEqual(result, expected);
+  });
+});
