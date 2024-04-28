af3f36d18919b0c3cf47b4dbd6a21dda27c7a1e3
Zia Roque <zia.roqs@gmail.com>
2024-04-28 01:02:31 -0400

    test: Refactor Mocha test files for problems 1 to 3

--- test/03-factorial-test.js
+++ test/03-factorial-test.js
@@ -2,57 +2,16 @@ const assert = require('assert');
 const factorial = require('../solutions/03-factorial.js');
 
 describe('factorial', () => {
-  it('tests if factorial(5) returns 120', () => {
-    const num = 5;
-    const expected = 120;
+  const tests = ({ num, expected }) =>
+    function () {
+      const result = factorial(num);
+      assert.strictEqual(result, expected);
+    };
 
-    const result = factorial(num);
-
-    assert.strictEqual(result, expected);
-  });
-
-  it('tests if factorial(4) returns 24', () => {
-    const num = 4;
-    const expected = 24;
-
-    const result = factorial(num);
-
-    assert.strictEqual(result, expected);
-  });
-
-  it('tests if factorial(3) returns 6', () => {
-    const num = 3;
-    const expected = 6;
-
-    const result = factorial(num);
-
-    assert.strictEqual(result, expected);
-  });
-
-  it('tests if factorial(2) returns 2', () => {
-    const num = 2;
-    const expected = 2;
-
-    const result = factorial(num);
-
-    assert.strictEqual(result, expected);
-  });
-
-  it('tests if factorial(1) returns 1', () => {
-    const num = 1;
-    const expected = 1;
-
-    const result = factorial(num);
-
-    assert.strictEqual(result, expected);
-  });
-
-  it('tests if factorial(0) returns 1', () => {
-    const num = 0;
-    const expected = 1;
-
-    const result = factorial(num);
-
-    assert.strictEqual(result, expected);
-  });
+  it('should return 120 if the input is 5', tests({ num: 5, expected: 120 }));
+  it('should return 24 if the input is 4', tests({ num: 4, expected: 24 }));
+  it('should return 6 if the input is 3', tests({ num: 3, expected: 6 }));
+  it('should return 2 if the input is 2', tests({ num: 2, expected: 2 }));
+  it('should return 1 if the input is 1', tests({ num: 1, expected: 1 }));
+  it('should return 1 if the input is 0', tests({ num: 0, expected: 1 }));
 });
